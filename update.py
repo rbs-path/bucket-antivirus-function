d3b56922334edc9a7e50c33c552fe514bba80e79
Zsolt Fejer <zsolt.fejer@path.co.uk>
2024-01-29 19:00:07 +0000

    unused

--- update.py
+++ update.py
@@ -24,7 +24,6 @@ from common import AV_DEFINITION_S3_PREFIX
 from common import CLAMAVLIB_PATH
 from common import S3_ENDPOINT
 from common import get_timestamp
-import subprocess
 
 
 def lambda_handler(event, context):
